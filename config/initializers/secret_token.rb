4f0c2383b7d04757151908ee87c9648fc1bb943d
Ecenta <delta@ubuntu.(none)>
2013-05-02 17:18:15 +0800

    add Post

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = '6a003255b2a211fef0a0ea193ab19d036612b65ed06243ec6553e032db79dbcc2984100fc216755740bdaae30e1cf8c284dd515876175c3d7f3e88b62c5f2020'
